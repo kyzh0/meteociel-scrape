c59394cf6484052565285150c7fb4bd789681bfc
Kyle Zhou <kzkyle2000@gmail.com>
2024-09-13 14:30:34 +0200

    basic table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Date</TableCell>
+          <TableCell>Time</TableCell>
+          <TableCell>Temp</TableCell>
+          <TableCell>.</TableCell>
+          <TableCell>Avg</TableCell>
+          <TableCell>Gust</TableCell>
+          <TableCell>1500m</TableCell>
+          <TableCell>2000m</TableCell>
+          <TableCell>3000m</TableCell>
+          <TableCell>4200m</TableCell>
+          <TableCell>5600m</TableCell>
+          <TableCell>Avg</TableCell>
+          <TableCell>Gust</TableCell>
+          <TableCell>1500m</TableCell>
+          <TableCell>2000m</TableCell>
+          <TableCell>3000m</TableCell>
+          <TableCell>4200m</TableCell>
+          <TableCell>5600m</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell>Ven. 13</TableCell>
+          <TableCell>1400</TableCell>
+          <TableCell>12</TableCell>
+          <TableCell>SUN</TableCell>
+          <TableCell>5</TableCell>
+          <TableCell>10</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>1</TableCell>
+          <TableCell>1</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
   );
 }
 
